5edafc700d0e282e5da7aacf66eb7f5f82810709
gabrielhermont <138725279+gabrielhermont@users.noreply.github.com>
2023-11-23 13:09:53 -0300

    feat: Adding initial board

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,5 +1,35 @@
-import { Container } from "./Styles";
+import {
+  Container,
+  Titulo,
+  Nome,
+  Upload,
+  Descricao,
+  Input_nome,
+  Input_upload,
+  Input_descricao,
+  Salvar,
+  Quadro,
+} from "./Styles";
 
 export default function Favorites() {
-  return <Container>Favorites</Container>;
+  return (
+    <Container>
+      <Quadro>
+        <Titulo>CRIAR NOVA FERRAMENTA</Titulo>
+        <Nome>
+          Nome
+          <Input_nome placeholder='GPT'></Input_nome>
+        </Nome>
+        <Upload>
+          Upload de Imagem
+          <Input_upload placeholder='http//google'></Input_upload>
+        </Upload>
+        <Descricao>
+          Descrição curta
+          <Input_descricao placeholder='Escreva aqui a sua descrição'></Input_descricao>
+        </Descricao>
+        <Salvar>Salvar</Salvar>
+      </Quadro>
+    </Container>
+  );
 }
--- src/pages/Favorites/Styles.js
+++ src/pages/Favorites/Styles.js
@@ -5,3 +5,62 @@ export const Container = styled.div`
   flex-direction: column;
   margin-top: 150px;
 `;
+
+export const Titulo = styled.h1`
+  margin-left: 15px;
+`;
+
+export const Quadro = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const Nome = styled.label`
+  display: flex;
+  flex-direction: column;
+  width: 85%;
+`;
+
+export const Input_nome = styled.input`
+  background-color: black;
+  border: 2px solid white;
+  border-radius: 5px;
+`;
+
+export const Upload = styled.label`
+  display: flex;
+  flex-direction: column;
+  width: 85%;
+`;
+
+export const Input_upload = styled.input`
+  background-color: black;
+  border: 2px solid white;
+  border-radius: 5px;
+`;
+
+export const Descricao = styled.label`
+  display: flex;
+  flex-direction: column;
+  width: 85%;
+`;
+
+export const Input_descricao = styled.input`
+  background-color: black;
+  border: 2px solid white;
+  border-radius: 5px;
+`;
+
+export const Salvar = styled.button`
+  display: flex;
+  width: 150px;
+  height: 30px;
+  justify-content: center;
+  background-color: #013c9d;
+  border: 0px;
+  border-radius: 5px;
+  color: white;
+  font
+`;
