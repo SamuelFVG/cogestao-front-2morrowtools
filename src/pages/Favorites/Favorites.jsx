978b017450006e3a2d8f3775ca3e76991c203874
Lucas Velloso <lucasvelloso@cpejr.com.br>
2023-11-25 17:16:43 -0300

    submit

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,5 +1,41 @@
-import { Container } from "./Styles";
+import { useState } from "react";
+import { Container, DivNovaFerramenta, DivFormulario, Formulario, LabelFormulario, InputFormulario, BotaoFormulario, DivBotao} from "./Styles";
 
 export default function Favorites() {
-  return <Container>Favorites</Container>;
+const [Nome, setNome] = useState('');
+const [Imagem, setImagem] = useState('');
+const [Descricao, setDescricao] = useState('');
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+}
+
+  return (
+    <Container>
+      <DivNovaFerramenta>CRIAR NOVA FERRAMENTA</DivNovaFerramenta>
+      <DivFormulario>
+        <Formulario onSubmit={handleSubmit}>
+          <LabelFormulario>Nome</LabelFormulario>
+          <InputFormulario type="text" required placeholder="GPT" 
+          id="nome" onChange={(e) => setNome(e.target.value)}></InputFormulario>
+
+          <LabelFormulario>Upload de imagem</LabelFormulario>
+          <InputFormulario type= "url" required placeholder="http//google" 
+          id="imagem" onChange={(e) => setImagem(e.target.value)} ></InputFormulario>
+
+          <LabelFormulario>Descrição curta</LabelFormulario>
+          <InputFormulario type="text" required placeholder="Escreva aqui a sua descrição"
+          id= "descricao" onChange={(e) => setDescricao(e.target.value)} ></InputFormulario>
+        <DivBotao>
+        <BotaoFormulario>SALVAR</BotaoFormulario>
+        </DivBotao>
+        </Formulario>
+
+      </DivFormulario>
+
+      
+    </Container>
+  )
+
 }
--- src/pages/Favorites/Styles.js
+++ src/pages/Favorites/Styles.js
@@ -5,3 +5,71 @@ export const Container = styled.div`
   flex-direction: column;
   margin-top: 150px;
 `;
+
+export const DivNovaFerramenta = styled.div`
+ display: flex;
+ flex-direction: column;
+ width: 100%;
+ color: #fff;
+ font-size: 50px;
+ font-style: normal;
+ font-weight: 600;
+ line-height: normal;
+ padding-left: 4%;
+`
+
+export const DivFormulario = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+padding-left: 4%;
+padding-right: 4%;
+padding-top: 2%;
+`
+export const Formulario = styled.form`
+`
+export const LabelFormulario = styled.label`
+color: #fff;
+font-size: 22px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+text-align: left;
+display: block;
+margin-top: 2%;
+`
+export const InputFormulario = styled.input`
+width: 100%;
+height: 64px;
+margin-top: 1%;
+padding-left: 1%;
+background-color: transparent;
+border-radius: 6px;
+border: 2px solid #fff;
+color: #b4b4b4;
+font-size: 22px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+`
+
+export const DivBotao = styled.div` 
+display:flex;
+padding-top: 5%;
+align-items: center;
+justify-content: center;
+`
+
+export const BotaoFormulario = styled.button` 
+background-color: #013c9d;
+border-radius: 6px;
+border: transparent;
+width: 208px;
+height: 56px;
+flex-shrink: 0;
+text-align: center;
+color: #fff;
+font-size: 24px;
+font-weight: 400;
+line-height: normal;
+`
