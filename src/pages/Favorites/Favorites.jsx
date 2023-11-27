ce420b0c7843e9c5a0a4429e8b867d265eb57c5f
Mateus Bretz <mateusbretz@cpejr.com.br>
2023-11-26 22:06:33 -0300

    Tabela para informações-ferramentas

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,5 +1,36 @@
-import { Container } from "./Styles";
+import {
+  Container,
+  DivTexto,
+  Nome,
+  CorpoNome,
+  Gpt,
+  InfoFerramentas,
+  Link,
+  CorpoLink,
+  Url,
+  Descricao,
+  CorpoDescricao,
+  EscrevaDescricao,
+} from "./Styles";
 
 export default function Favorites() {
-  return <Container>Favorites</Container>;
+  return (
+    <Container>
+      <InfoFerramentas>
+        <DivTexto>CRIAR NOVA FERRAMENTA</DivTexto>
+        <Nome>Nome</Nome>
+        <CorpoNome>
+          <Gpt>GPT</Gpt>
+        </CorpoNome>
+        <Link>Upload de Imagem</Link>
+        <CorpoLink>
+          <Url>http//google</Url>
+        </CorpoLink>
+        <Descricao>Descrição curta</Descricao>
+        <CorpoDescricao>
+          <EscrevaDescricao>Escreva aqui a sua descrição</EscrevaDescricao>
+        </CorpoDescricao>
+      </InfoFerramentas>
+    </Container>
+  );
 }
--- src/pages/Favorites/Styles.js
+++ src/pages/Favorites/Styles.js
@@ -3,5 +3,86 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  width: 1440px;
+  height: 2834px;
+  position: fixed;
+`;
+
+export const InfoFerramentas = styled.div``;
+
+export const DivTexto = styled.div`
+  display: flex;
+  flex-direction: column;
   margin-top: 150px;
+  margin-left: 3%;
+  font-size: 30px;
+`;
+
+export const Nome = styled.div`
+  display: flex;
+  margin-top: 20px;
+  margin-left: 3%;
+`;
+
+export const CorpoNome = styled.div`
+  display: flex;
+  margin-left: 3%;
+  margin-top: 5px;
+  border-radius: 6px;
+  border: 2px solid #fff;
+  height: 50px;
+  width: 85%;
+  padding-left: 20px;
+  align-items: center;
+`;
+
+export const Gpt = styled.div`
+  display: flex;
+  color: #b4b4b4;
+`;
+
+export const Link = styled.div`
+  display: flex;
+  margin-top: 20px;
+  margin-left: 3%;
+`;
+
+export const CorpoLink = styled.div`
+  display: flex;
+  margin-left: 3%;
+  margin-top: 5px;
+  border-radius: 6px;
+  border: 2px solid #fff;
+  height: 50px;
+  width: 85%;
+  padding-left: 20px;
+  align-items: center;
+`;
+
+export const Url = styled.div`
+  display: flex;
+  color: #b4b4b4;
+`;
+
+export const Descricao = styled.div`
+  display: flex;
+  margin-top: 20px;
+  margin-left: 3%;
+`;
+
+export const CorpoDescricao = styled.div`
+  display: flex;
+  margin-left: 3%;
+  margin-top: 5px;
+  border-radius: 6px;
+  border: 2px solid #fff;
+  height: 50px;
+  width: 85%;
+  padding-left: 20px;
+  align-items: center;
+`;
+
+export const EscrevaDescricao = styled.div`
+  display: flex;
+  color: #b4b4b4;
 `;
