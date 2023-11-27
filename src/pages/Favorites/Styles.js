818683ee14641b19e21304502f9afef51ae8b859
Lara de Oliveira <laraffolivera@gmail.com>
2023-11-26 21:31:47 -0300

    começando

--- src/pages/Favorites/Styles.js
+++ src/pages/Favorites/Styles.js
@@ -5,3 +5,15 @@ export const Container = styled.div`
   flex-direction: column;
   margin-top: 150px;
 `;
+const tools = [
+  {
+    name: "ChatGPT",
+    description: "LLM da OpenAI",
+    imageUrl: "https://picsum.photos/id/237/536/354",
+  },
+  {
+    name: "Perplexity Ai",
+    description: "Ferramenta de pesquisa com integração de um LLM",
+    imageUrl: "https://picsum.photos/id/237/536/354",
+  },
+];
