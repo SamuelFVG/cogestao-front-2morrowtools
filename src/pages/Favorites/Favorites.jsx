f19504aa4ad4607159172cde3af5ac956ba630e3
Lara de Oliveira <laraffolivera@gmail.com>
2023-11-29 20:22:33 -0300

    teste

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,5 +1,16 @@
-import { Container } from "./Styles";
-
+import { string } from "prop-types";
+import { Container, Title1, Firstpart, InputArea } from "./Styles";
+import { Input } from "antd";
 export default function Favorites() {
-  return <Container>Favorites</Container>;
+  return (
+    <Container>
+      <Firstpart>
+        <Title1>CRIAR NOVA FERRAMENTA</Title1>
+        <InputArea>
+          Nome
+          <Input placeholder='nome' />
+        </InputArea>
+      </Firstpart>
+    </Container>
+  );
 }
--- src/pages/Favorites/Styles.js
+++ src/pages/Favorites/Styles.js
@@ -5,15 +5,57 @@ export const Container = styled.div`
   flex-direction: column;
   margin-top: 150px;
 `;
-const tools = [
-  {
-    name: "ChatGPT",
-    description: "LLM da OpenAI",
-    imageUrl: "https://picsum.photos/id/237/536/354",
-  },
-  {
-    name: "Perplexity Ai",
-    description: "Ferramenta de pesquisa com integração de um LLM",
-    imageUrl: "https://picsum.photos/id/237/536/354",
-  },
-];
+
+export const Title1 = styled.div`
+  font-family: Exo 2;
+  display: block;
+  white-space: nowrap;
+  color: #fff;
+  font-size: 50px;
+  font-weight: 600;
+  line-height: 60px;
+  width: 610px;
+  height: 60px;
+  padding-top: 2rem;
+  padding-left: 7rem;
+`;
+export const Firstpart = styled.div`
+  /* justify-content: center;
+  align-items: center; */
+  display: flex;
+  align-items: left;
+`;
+
+// export const Nome = styled.div`
+//   font-family: Roboto;
+//   font-size: 22px;
+//   font-weight: 400;
+//   line-height: 26px;
+//   display: flex;
+//   width: 258.14px;
+//   height: 28px;
+//   padding-top: 5rem;
+//   padding-left: 7rem;
+// `;
+// // export const Campo = styled.div`
+//   font-weight: 500;
+// `;
+
+// export const Form = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 2rem;
+//   width: 80vw;
+//   height: 22rem;
+//   border-radius: 50px 0px;
+//   border: 1px solid white;
+//   margin-top: 4rem;
+//   margin-bottom: 4rem;
+//   font-weight: 500;
+// `;
+
+export const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
